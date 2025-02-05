77cdf05e08d0aaccb53a482d5688d2732f955c1a
eagle-edo-dsbm <dsbmmusicguitar@gmail.com>
2025-02-05 12:41:50 +0200

    todolist

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const newTaskInput = document.getElementById("newTask");
+const submitButton = document.querySelector(".submit");
+const taskContainer = document.querySelector(".tdl-container");
+const taskList = document.querySelector('.task-list');
+const errorMessage = document.querySelector('.error-message');
+
+submitButton.addEventListener('click', function(){
+    const newTask = newTaskInput.value.trim();
+    if(newTask !== ""){
+        errorMessage.style.display='none';
+        addTaskToList(newTask);
+        newTaskInput.value = "";
+    }
+    else{
+        errorMessage.style.display='block';
+    }
+
+    function addTaskToList(newTask){
+        const taskElement = document.createElement('div');
+        taskElement.textContent = newTask;
+        taskList.appendChild(taskElement);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Удалить';
+        deleteButton.classList.add('delete-button');
+        taskElement.appendChild(deleteButton);
+
+        const doneButton = document.createElement('button');
+        doneButton.textContent = 'Выполненно!';
+        doneButton.classList.add('done-button');
+        taskElement.appendChild(doneButton);
+
+        deleteButton.addEventListener('click', function(){
+            taskElement.remove();
+        });
+
+        doneButton.addEventListener('click', function(){
+            if(taskElement.style.color === 'green' && taskElement.style.fontWeight === 'bold'){
+                taskElement.style.color = '';
+                taskElement.style.fontWeight = '';
+            }
+            else{
+                taskElement.style.color = 'green';
+                taskElement.style.fontWeight = 'bold';
+            }
+        })
+    }
+});
